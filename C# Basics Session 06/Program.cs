de61884f732536c889fff09ffb60c25a9ebbafcb
Mahmoud Mansour <mansour@gmail.com>
2025-07-18 11:48:22 +0300

    Complete First Video - Part 01 1D Array

--- C# Basics Session 06/Program.cs
+++ C# Basics Session 06/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Arrays
+
+            #region 1D Array
+
+            #region Example 01
+
+            //int x = 0;
+            //int y = 1;
+            //int z = 2;
+            //int d = 3;
+
+            //int[] Numbers;
+
+            //Numbers = new int[3];
+
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[2]);
+
+            //Numbers[0] = 1;
+            //Numbers[1] = 2;
+            //Numbers[2] = 3;
+
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[2]);
+
+            //Console.WriteLine($"Length of Array is {Numbers.Length}");
+            //Console.WriteLine($"Rank of Array is {Numbers.Rank}");
+
+            //Console.WriteLine($"Hash Code of Array is {Numbers.GetHashCode()}");
+            //Console.WriteLine($"Hash Code of Array Index '0' is {Numbers[0].GetHashCode()}");
+            //Console.WriteLine($"Hash Code of Array Index '1' is {Numbers[1].GetHashCode()}");
+            //Console.WriteLine($"Hash Code of Array Index '2' is {Numbers[2].GetHashCode()}");
+
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+
+            //foreach (int item in Numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            #endregion
+
+            #region Array Creation Way
+
+            //int[] Numbers01 = new int[3];
+            //int[] Numbers02 = new int[] {1, 2, 3};
+            //int[] Numbers03 = new int[3] {1, 2, 3};
+            //int[] Numbers04 = { 1, 2, 3 };
+
+            #endregion
+
+            #endregion
+
+            #endregion
         }
     }
 }
